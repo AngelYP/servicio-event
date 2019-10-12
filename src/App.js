c28ce3f304a9d2db6ccf61f360f3d6a19a02dfec
AngelYP <angel.yaperez@gmail.com>
2019-10-12 18:44:44 -0500

    dependeces and table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table'
+import { Grid } from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Grid container>
+            <Grid item xs={12}>
+                <Table/>
+            </Grid>
+        </Grid>
+    );
 }
 
 export default App;
